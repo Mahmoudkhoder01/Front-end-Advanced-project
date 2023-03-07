1cea2ab9fb5eea51161ee2323820012ef8b3fc07
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-03-07 14:52:20 +0200

    d

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -1,16 +1,17 @@
 import classes from "./card.module.css";
 import { BiUserCircle } from "react-icons/bi";
 
-function Card() {
+function Card(props) {
   return (
     <div className={classes.card}>
       <h2>
         <BiUserCircle size={30} />
-        Number of Admins
+        Number of {props.name}
       </h2>
-      <span>18</span>
+      {props.number.length === 1 ? <p>{props.number}</p> : <span>{props.number}</span>}
+        {/* <span>{props.number}</span> */}
     </div>
   );
 }
 
-export default Card;
+export default Card
--- src/Pages/Dashboard/dashboard.js
+++ src/Pages/Dashboard/dashboard.js
@@ -1,14 +1,16 @@
 import Card from "../../Components/Card/card";
+import Chart from "../../Components/Chart/chart";
 import classes from "./dashboard.module.css";
 
 function Dashboard() {
   return (
     <div className={classes.dashboard}>
       <div className={classes.cards}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+        <Card name={"Admins"} number={"18"} />
+        <Card name={"Classes"} number={"7"} />
+        <Card name={"Sections"} number={"12"} />
+        <Card name={"Students"} number={"47"} />
+        <Chart />
       </div>
     </div>
   );
