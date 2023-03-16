d870af88150ea4abd95f5292a658bdc982dee210
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-03-16 16:04:35 +0200

    finish_crud_class

--- src/Pages/ClassRoom/classroom.js
+++ src/Pages/ClassRoom/classroom.js
@@ -1,83 +1,115 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import { DataGrid } from "@mui/x-data-grid";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loading from "../../Components/Loading/loading";
+import AdminEditCard from "../../Components/AdminCard/adminEditCard";
+import AdminDeleteCard from "../../Components/AdminCard/adminDeleteCard";
+import AddAdminForm from "../../Components/addAdmin/addAdmin";
+import Pagination from "../../Components/paginantion/pagination";
+import Classes from './classroom.module.css'
+import AddClassForm from "../../Components/addclass/addclass";
+import ClassEditCard from "../../Components/editclass/editclass";
+import ClassDeleteCard from "../../Components/deleteclass/deleteclass";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "grade",
-    headerName: "grade",
-    width: 150,
-    editable: true,
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#5541D7",
+    color: theme.palette.common.white,
   },
-  {
-    field: "icon",
-    headerName: "icon",
-    width: 150,
-    editable: true,
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
   },
-];
+}));
 
-const rows = [
-  { id: 1, grade: "Snow", icon: "Jon" },
-  { id: 2, grade: "Lannister", icon: "Cersei" },
-  { id: 3, grade: "Lannister", icon: "Jaime" },
-  { id: 4, grade: "Stark", icon: "Arya" },
-  { id: 5, grade: "Targaryen", icon: "Daenerys" },
-  { id: 6, grade: "Melisandre", icon: "null" },
-  { id: 7, grade: "Clifford", icon: "Ferrara" },
-  { id: 8, grade: "Frances", icon: "Rossini" },
-  { id: 9, grade: "Roxie", icon: "Harvey" },
-];
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
-export default function DataGridDemo() {
+const FixedTables = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/grade");
+      setData(response.data.message);
+      setIsLoading(true);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
   return (
-    <div
-      sx={{
-        color: "success.dark",
-        display: "inline",
-        fontWeight: "bold",
-        mx: 0.5,
-        fontSize: 14,
-        paddingLeft: '100rem',
-        background:"white"
-      }}
-    >
-      <Box
-        sx={{
-          height: 400,
-          width: "50%",
-        }}
-      >
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
+    <>
+      {isLoading ? (
+        <>
+          <AddClassForm regetData={fetchData}/>
+          <TableContainer
+            className={Classes.adminPage}
+            component={Paper}
+            initialState={{
+              pagination: {
+                paginationModel: { pageSize: 10, page: 0 },
               },
-            },
-          }}
-          pageSizeOptions={[5]}
-          checkboxSelection
-          disableRowSelectionOnClick
-          sx={
-            {
-              // background: "white",
-              // display:"flex",
-              // justifyContent:"center",
-              // alignContent:"center",
-              // color: 'success.dark',
-              // display: 'inline',
-              // fontWeight: 'bold',
-              // mx: 0.5,
-              // fontSize: 14,
-              // paddingLeft:25,
-            }
-          }
-        />
-      </Box>
-    </div>
+            }}
+          >
+            <Table>
+              <TableHead>Classes</TableHead>
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>ID</StyledTableCell>
+                  <StyledTableCell>NAME</StyledTableCell>
+                  <StyledTableCell>CRAETED AT</StyledTableCell>
+                  <StyledTableCell>UPDATET AT</StyledTableCell>
+                  <StyledTableCell></StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((row) => (
+                  <StyledTableRow key={row.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.id}
+                    </StyledTableCell>
+                    <StyledTableCell>{row.name}</StyledTableCell>
+                    <StyledTableCell>{row.created_at}</StyledTableCell>
+                    <StyledTableCell>{row.updated_at}</StyledTableCell>
+                    <StyledTableCell style={{ display: "flex" }}>
+                      <ClassEditCard
+                        adminValue={row.name}
+                        rowId={row.id}
+                        regetData={fetchData}
+                      />
+                      <ClassDeleteCard rowId={row.id} regetData={fetchData} />
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Pagination />
+        </>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
-}
+};
+
+export default FixedTables;
