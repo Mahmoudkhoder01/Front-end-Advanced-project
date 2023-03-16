ed0ec0e1e9a52a9d89e4e9bdc932abc5ca3f01d7
taghrid <taghridtaleb1@gmail.com>
2023-03-16 15:37:12 +0200

    ww

--- src/Pages/ClassRoom/classroom.js
+++ src/Pages/ClassRoom/classroom.js
@@ -5,19 +5,23 @@ import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from "@mui/icons-material/Edit";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import GradeIcon from '@mui/icons-material/Grade';
 import Fab from "@mui/material/Fab";
 import Tooltip from "@mui/material/Tooltip";
 import "./classroom.css";
-import postData from "./create.js";
-// import Read from "./read.js";
-import Update from "./update";
-import { Button, Form } from "semantic-ui-react";
-import { FormControl } from '@mui/material';
-import { InputLabel } from '@mui/material';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  Button,
+} from "@mui/material";
 
-
-
+//const funct = () => axios.create({
+//   baseURL: "http://localhost:8080/api",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
 
 function formatDate(dateString) {
   const createdDate = new Date(dateString);
@@ -28,103 +32,105 @@ function formatDate(dateString) {
   })} 
   ${createdDate.toLocaleDateString("en-US")}`;
 }
-const columns = [
-  // { field: 'id', headerName: 'ID', width: 70 },
-
-  {
-    field: "name",
-    headerName: "Grades",
-    width: 800,
-    editable: true,
-    key: "name",
-    renderCell: (params) => (
-      <Tooltip
-        title={
-          <h1 style={{ fontSize: "1rem" }}>
-            Created @ {formatDate(params.row.created_at)}
-          </h1>
-        }
-        arrow
-        placement="right"
-      >
-        <div
-          style={{
-            fontWeight: "bolder",
-            marginLeft: "4rem",
-            fontSize: "1.25rem",
-          }}
-        >
-          {params.value}
-        </div>
-      </Tooltip>
-    ),
-  },
-
-  {
-    field: "delete",
-    headerName: "",
-    type: "number",
-    width: 100,
-    align: "center",
-    editable: true,
-    sortable: false,
-    key: "delete",
-    renderCell: (params) => (
-      <Fab color="black" size="small" aria-label="delete">
-        <DeleteForeverIcon
-          style={{
-            cursor: "pointer",
-          }}
-          onMouseEnter={(e) => {
-            e.target.style.color = "red";
-          }}
-          onMouseLeave={(e) => {
-            e.target.style.color = "black";
-          }}
-          onClick={onClickDelete}
-          
-        />
-      </Fab>
-    ),
-  },
-
-  {
-    field: "edit",
-    headerName: "",
-    width: 100,
-    editable: true,
-    align: "center",
-    sortable: false,
-    key: "edit",
-    renderCell: (params) => (
-      <Fab color="blue" size="small" aria-label="edit">
-        <EditIcon
-        //  onClick={() =>    }
-        />
-      </Fab>
-    ),
-  },
-  {
-    field: "add",
-    headerName: "",
-    width: 100,
-    sortable: false,
-    key: "add",
-    renderHeader: () => (
-      <Fab color="blue" size="large" aria-label="add">
-        <AddIcon
-          style={{ fontSize: "2.5rem", fontWeight: "bolder", margin: "2rem" }}
-          onClick={() => {
-            document.querySelector("#modal").showModal();
-          }}
-        />
-      </Fab>
-    ),
-  },
-];
 
 export default function Classroom() {
   const [names, setNames] = useState([]);
+
+  const columns = [
+    {
+      field: "name",
+      headerName: "Grades",
+      width: 800,
+      editable: true,
+      key: "name",
+      renderCell: (params) => (
+        <Tooltip
+          title={
+            <h1 style={{ fontSize: "1rem" }}>
+              Created @ {formatDate(params.row.created_at)}
+            </h1>
+          }
+          arrow
+          placement="right"
+        >
+          <div
+            style={{
+              fontWeight: "bolder",
+              marginLeft: "4rem",
+              fontSize: "1.25rem",
+            }}
+          >
+            {params.value}
+          </div>
+        </Tooltip>
+      ),
+    },
+
+    {
+      field: "delete",
+      headerName: "",
+      type: "number",
+      width: 100,
+      align: "center",
+      editable: true,
+      sortable: false,
+      key: "delete",
+      renderCell: (params) => (
+        <Fab color="black" size="small" aria-label="delete">
+          <DeleteForeverIcon
+            style={{
+              cursor: "pointer",
+            }}
+            onMouseEnter={(e) => {
+              e.target.style.color = "red";
+            }}
+            onMouseLeave={(e) => {
+              e.target.style.color = "black";
+            }}
+            onClick={() => {
+              deleteName(params.id);
+            }}
+            //  () =>showDeleteModal()
+            //  ${params.id}
+          />
+        </Fab>
+      ),
+    },
+
+    {
+      field: "edit",
+      headerName: "",
+      width: 100,
+      editable: true,
+      align: "center",
+      sortable: false,
+      key: "edit",
+      renderCell: (params) => (
+        <Fab color="blue" size="small" aria-label="edit">
+          <EditIcon
+          // onClick={() =>  ()  }
+          />
+        </Fab>
+      ),
+    },
+    {
+      field: "add",
+      headerName: "",
+      width: 100,
+      sortable: false,
+      key: "add",
+      renderHeader: () => (
+        <Fab color="blue" size="large" aria-label="add">
+          <AddIcon
+            style={{ fontSize: "2.5rem", fontWeight: "bolder", margin: "2rem" }}
+            onClick={() => {
+              document.querySelector("#add").showModal();
+            }}
+          />
+        </Fab>
+      ),
+    },
+  ];
 
   const fetchData = async () => {
     try {
@@ -161,18 +167,33 @@ export default function Classroom() {
     }
   };
 
-  const onDelete = () => {
-   
+  const editData = async (id) => {
+    try {
+      const response = await axios.put(`http://localhost:8000/api/grade/${id}`, {
+        name,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/grade/{id}`).then((response) => {
+      // setPost(response.data);
+    });
+  }, []);
 
-  // const onClickDelete = () => {
-  //   () => {
-  //     alert();
-  //     onDelete();
-  //   }
-  // };
 
+  const deleteName = async (id) => {
+    try {
+      const response = await axios.delete(`http://localhost:8000/api/grade/${id}`);
+      alert("Post deleted!");
+      setName(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   if (!names) return <h1>loading</h1>;
   return (
     <Box
@@ -189,21 +210,55 @@ export default function Classroom() {
         getRowId={(name) => name.id}
       />
 
+      <dialog id="add">
+        <FormControl>
+          <InputLabel htmlFor="my-input">
+            Please enter the new grade here
+          </InputLabel>
+          <Input
+            id="my-input"
+            aria-describedby="my-helper-text"
+            placeholder="Grade"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <FormHelperText id="my-helper-text">
+            You are adding a new grade.
+          </FormHelperText>
+          <Button
+            onClick={postData}
+            type="submit"
+            variant="contained"
+            size="small"
+          >
+            Add
+          </Button>
+        </FormControl>
+      </dialog>
 
-
-<FormControl>
-  <InputLabel htmlFor="my-input">Email address</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-</FormControl>
-
-
-
-
-
-
-
-
+      <dialog id="edit">
+        <FormControl>
+          <InputLabel htmlFor="my-input">
+            Please enter the new grade here
+          </InputLabel>
+          <Input
+            id="my-input"
+            aria-describedby="my-helper-text"
+            placeholder="Grade"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <FormHelperText id="my-helper-text">
+            You are adding a new grade.
+          </FormHelperText>
+          <Button
+            onClick={editData}
+            type="submit"
+            variant="contained"
+            size="small"
+          >
+            Add
+          </Button>
+        </FormControl>
+      </dialog>
     </Box>
   );
 }
