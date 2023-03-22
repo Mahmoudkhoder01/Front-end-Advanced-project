85ba3d634e246c3fb896f886d340fff849273d98
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-03-22 10:15:03 +0200

    ii

--- src/Components/Topbar/topbar.js
+++ src/Components/Topbar/topbar.js
@@ -1,13 +1,28 @@
 import classes from "./topbar.module.css";
 import { useLocation } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function Topbar() {
   if (useLocation().pathname === "/") return null;
+
+  const authCookie = Cookies.get("auth");
+  let adminName = "";
+  if (!authCookie) {
+  } else {
+    try {
+      const dataUser = JSON.parse(authCookie);
+      console.log(dataUser);
+      const token = dataUser.access_token;
+      adminName = dataUser.user.name;
+    } catch (error) {
+      console.error("Invalid auth cookie:", authCookie);
+    }
+  }
   return (
     <div className={classes.topbar}>
       <div className={classes.admin}>
         <div className={classes.end}>
-          <h4>Mahmoud khodor</h4>
+          <h4>{adminName}</h4>
         </div>
       </div>
     </div>
